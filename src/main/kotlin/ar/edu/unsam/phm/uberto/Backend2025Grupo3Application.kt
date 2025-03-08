51f6e7477f9bcc20fcc7d2ddd52945d781abd27d
AdrianPerez0306 <perezadrian.m@hotmail.com>
2025-03-08 11:28:37 -0300

    Repo inicial

---
+++ src/main/kotlin/ar/edu/unsam/phm/uberto/Backend2025Grupo3Application.kt
@@ -0,0 +1,11 @@
+package ar.edu.unsam.phm.uberto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Backend2025Grupo3Application
+
+fun main(args: Array<String>) {
+	runApplication<Backend2025Grupo3Application>(*args)
+}
---
+++ src/test/kotlin/ar/edu/unsam/phm/uberto/Backend2025Grupo3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package ar.edu.unsam.phm.uberto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Backend2025Grupo3ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
