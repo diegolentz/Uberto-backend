2698dbaf95f24550fe3501af2c949969f52db508
Diaz Matias Hernan <diaz.matiash@gmail.com>
2025-03-22 19:14:06 -0300

    Se cambiar respuesta de creacion por ResponseEntity

--- src/main/kotlin/ar/edu/unsam/phm/uberto/controller/TripController.kt
+++ src/main/kotlin/ar/edu/unsam/phm/uberto/controller/TripController.kt
@@ -5,7 +5,9 @@ import ar.edu.unsam.phm.uberto.model.Trip
 import ar.edu.unsam.phm.uberto.services.TravelTimeMockService
 import ar.edu.unsam.phm.uberto.services.TripService
 import ar.edu.unsam.phm.uberto.services.UserService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.function.EntityResponse
 
 @CrossOrigin(origins = ["http://localhost:8080", "http://localhost:5173"])
 @RestController
@@ -17,7 +19,7 @@ class TripsController(private val travelTimeService: TravelTimeMockService, priv
     }
 
     @PostMapping("/create")
-    fun createTrip(@RequestBody trip: TripDTO): TripDTO {
+    fun createTrip(@RequestBody trip: TripDTO): ResponseEntity<String> {
         return tripService.createTrip(trip)
     }
 
