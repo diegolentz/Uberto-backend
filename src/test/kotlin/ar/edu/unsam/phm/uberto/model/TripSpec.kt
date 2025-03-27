122ad6a2b604c7cd9a4e0dddcb5780b9f34c3c2e
AdrianPerez0306 <perezadrian.m@hotmail.com>
2025-03-27 14:41:48 -0300

    Test de viajes. Cambio de nombre de metodo para eliminar score del viaje trip.delete() por trip.deleteScore()

--- src/test/kotlin/ar/edu/unsam/phm/uberto/model/TripSpec.kt
+++ src/test/kotlin/ar/edu/unsam/phm/uberto/model/TripSpec.kt
@@ -1,86 +1,112 @@
 package ar.edu.unsam.phm.uberto.model
 //
 import ar.edu.unsam.phm.uberto.BalanceAmmountNotValidException
-import ar.edu.unsam.phm.uberto.InsufficientBalanceException
+import ar.edu.unsam.phm.uberto.ScoredTripException
 import ar.edu.unsam.phm.uberto.TripNotFinishedException
-import ar.edu.unsam.phm.uberto.builder.PassengerBuilder
+
 import ar.edu.unsam.phm.uberto.builder.TripBuilder
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.date.shouldBeAfter
+import io.kotest.matchers.date.shouldBeBefore
 import io.kotest.matchers.doubles.shouldBeExactly
+import io.kotest.matchers.equals.shouldBeEqual
 import io.kotest.matchers.shouldBe
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
 
+
 class TripSpec: DescribeSpec({
     isolationMode = IsolationMode.InstancePerTest
+    var trip: Trip = TripBuilder().build()
+    describe(name="Given a trip"){
 
-    var usuario: Passenger = PassengerBuilder().build()
-    val simpleDriver: SimpleDriver = SimpleDriver()
-    var trip    : Trip = TripBuilder().driver(simpleDriver).build()
-
-    describe(name="Dado un viaje"){
-        it(name="Su balance inicial es 0"){
-            usuario.balance shouldBeExactly 0.0
+        it(name="Start date") {
+            val date:LocalDateTime = LocalDateTime.now()
+            var trip: Trip = TripBuilder().setDate(date.toString()).build()
+            trip.date shouldBeEqual date
         }
 
-        describe(name="Puede agregar dinero") {
-            it(name="Balance positivo se ve reflejado"){
-                usuario.loadBalance(5000.0)
-                usuario.balance shouldBe 5000
-            }
-
-            it(name="Balance negativo NO se puede"){
-                shouldThrow<BalanceAmmountNotValidException> {
-                    usuario.loadBalance(-1.0)
-                }
-            }
-
-        }
-        describe(name="Puede viajar si puede pagar el viaje") {
-            val simpleDriver:SimpleDriver = SimpleDriver()
-            simpleDriver.basePrice = 10.0
-            val trip:Trip = TripBuilder().duration(10).passenger(usuario).driver(simpleDriver).build()
-            it(name="Balance suficiente"){
-                val balance:Double = 1000000.0
-                usuario.loadBalance(balance)
-                usuario.requestTrip(trip)
-                usuario.balance shouldBeExactly (balance - trip.price())
-            }
-
-            it(name="Balance insuficiente"){
-                shouldThrow<InsufficientBalanceException> {
-                    usuario.requestTrip(trip)
-                }
-            }
-
+        it(name="Finalization date is start date plus duration") {
+            val date:LocalDateTime = LocalDateTime.now()
+            val duration:Int = 10
+            trip.duration = duration
+            trip.date = date
+            trip.finalizationDate() shouldBeEqual date.plus(duration.toLong(), ChronoUnit.MINUTES)
         }
 
-        describe(name="Puede calificar el viaje") {
-            val simpleDriver:SimpleDriver = SimpleDriver()
-            simpleDriver.basePrice = 10.0
-            val balance:Double = 1000000.0
-            usuario.loadBalance(balance)
-            it(name="Califica el viaje, si el viaje ya termino"){
-                val yesterday:LocalDateTime = LocalDateTime.now().minus(1, ChronoUnit.DAYS)
-                val trip:Trip = TripBuilder().duration(10).passenger(usuario).driver(simpleDriver).setDate(yesterday.toString()).build()
-                usuario.requestTrip(trip)
-                trip.finished() shouldBe true
-                usuario.scoreTrip(trip, message = "Score message", scorePoints = 5)
-
+        describe(name="State") {
+            it(name="Finished if finalization date is before now-instant"){
+                val today:LocalDateTime = LocalDateTime.now()
+                val yesterday:LocalDateTime = today.minus(1, ChronoUnit.DAYS)
+                trip.date = yesterday
+                trip.date shouldBeBefore today
+                trip.finished() shouldBeEqual true
             }
 
-            it(name="No puede calificar un viaje pendiente."){
-                val tomorrow:LocalDateTime = LocalDateTime.now().plus(1, ChronoUnit.DAYS)
-                val trip:Trip = TripBuilder().duration(10).passenger(usuario).driver(simpleDriver).setDate(tomorrow.toString()).build()
-                usuario.requestTrip(trip)
-                trip.finished() shouldBe false
+            it(name="Not Finished if finalization date is after now-instant"){
+                val today:LocalDateTime = LocalDateTime.now()
+                val tomorrow:LocalDateTime = today.plus(1, ChronoUnit.DAYS)
+                trip.date = tomorrow
+                trip.date shouldBeAfter today
+                trip.finished() shouldBeEqual false
+            }
+        }
+
+        describe(name="Score") {
+            it(name="No score by default"){
+                trip.score shouldBe null
+                trip.scored() shouldBe false
+            }
+
+            it(name="Can be scored if is finished"){
+                val today:LocalDateTime = LocalDateTime.now()
+                val yesterday:LocalDateTime = today.minus(1, ChronoUnit.DAYS)
+                trip.date = yesterday
+                trip.finished() shouldBeEqual true
+                val score:TripScore = TripScore(message = "Score message", scorePoints = 5)
+                trip.scored() shouldBeEqual false
+                trip.addScore(score)
+                trip.scored() shouldBeEqual true
+            }
+
+            it(name="Cannot be scored if is not finished"){
+                val today:LocalDateTime = LocalDateTime.now()
+                val tomorrow:LocalDateTime = today.plus(1, ChronoUnit.DAYS)
+                trip.date = tomorrow
+                trip.finished() shouldBeEqual false
+                val score:TripScore = TripScore(message = "Score message", scorePoints = 5)
                 shouldThrow<TripNotFinishedException> {
-                    usuario.scoreTrip(trip, message = "Score message", scorePoints = 5)
+                    trip.addScore(score)
                 }
             }
 
+            it(name="Cannot be scored if is already scored"){
+                val today:LocalDateTime = LocalDateTime.now()
+                val yesterday:LocalDateTime = today.minus(1, ChronoUnit.DAYS)
+                trip.date = yesterday
+                trip.finished() shouldBeEqual true
+                val score:TripScore = TripScore(message = "Score message", scorePoints = 5)
+                trip.addScore(score)
+                shouldThrow<ScoredTripException> {
+                    trip.addScore(score)
+                }
+            }
+
+            it(name="Score can be deleted"){
+                val today:LocalDateTime = LocalDateTime.now()
+                val yesterday:LocalDateTime = today.minus(1, ChronoUnit.DAYS)
+                trip.date = yesterday
+                trip.finished() shouldBeEqual true
+                val score:TripScore = TripScore(message = "Score message", scorePoints = 5)
+
+                trip.scored() shouldBeEqual false
+                trip.addScore(score)
+                trip.scored() shouldBeEqual true
+                trip.deleteScore()
+                trip.scored() shouldBeEqual false
+            }
         }
 
     }
