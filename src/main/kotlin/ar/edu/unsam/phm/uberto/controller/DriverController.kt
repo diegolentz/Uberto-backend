d1167618a774c837d240f3976d5fd2101a18be72
Matias Hernan Diaz <87882067+MatiasHernanDiaz@users.noreply.github.com>
2025-03-30 22:23:23 -0300

    Fix driver controller service (#70)
    * refactor EP avaliable
    
    * refactor EP getById de driver se pasa a path variable
    
    * refactor EP changeProfile
    
    * refactor EP getDriversAvailable pasa a get con pathvariable
    
    * refactor EP get img
    
    * se cambia redundancia en nombre de variables
    
    * actualizado
    
    * ingles
    
    * path variable para obtener 1 driver

--- src/main/kotlin/ar/edu/unsam/phm/uberto/controller/DriverController.kt
+++ src/main/kotlin/ar/edu/unsam/phm/uberto/controller/DriverController.kt
@@ -2,39 +2,45 @@ package ar.edu.unsam.phm.uberto.controller
 
 import ar.edu.unsam.phm.uberto.dto.*
 import ar.edu.unsam.phm.uberto.services.DriverService
+import ar.edu.unsam.phm.uberto.services.TravelTimeMockService
+import exceptions.BusinessException
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import kotlin.random.Random
+import java.time.LocalDateTime
 
 @CrossOrigin(origins = ["http://localhost:8080", "http://localhost:5173"])
 @RestController
 @RequestMapping("/driver")
-class DriverController(private val driverService: DriverService) {
+class DriverController(private val driverService: DriverService, val timeTripsService: TravelTimeMockService) {
 
-    @GetMapping()
-    fun getByID(@RequestParam driverId:Int): DriverDTO {
-        val driver = driverService.getDriverData(driverId)
+    @GetMapping("/{id}")
+    fun getByID(@PathVariable id:Int): DriverDTO {
+        val driver = driverService.getDriverData(id)
         return driver.toDTO()
     }
 
-    @PostMapping("/avaliable")
-    fun getDriversAvailable(@RequestBody date: DateDTO): DriverCardAndTimeDTO {
-        //aca tengo que retornar ya tipo DTO por el servicio de tiempos
-        return driverService.getDriversAvailable(date)
-    }
-
-    @GetMapping("/all") ///PARA TEST DE DESA
-    fun get(): List<DriverDTO> {
-        return driverService.getAllDrivers().map { it.toDTO() }
+    @GetMapping("/avaliable")
+    fun getDriversAvailable(@RequestParam date: LocalDateTime,
+                            @RequestParam origin: String,
+                            @RequestParam destination: String,
+                            @RequestParam numberpassengers: Int): DriverCardAndTimeDTO {
+        val timeMap = timeTripsService.getTime()
+        val time = timeMap["time"] ?: throw BusinessException("Failure in the time calculation system")
+        val avaliableDrivers = driverService.getDriversAvailable(date, time)
+        val driverCardDTO = avaliableDrivers.map{it.toCardDTO(timeMap["time"]!!, numberpassengers)}
+        return DriverCardAndTimeDTO(timeMap["time"]!!, driverCardDTO)
     }
 
     @PostMapping()
     fun changeProfile(@RequestBody driverDTO: DriverDTO): ResponseEntity<String>{
-        return driverService.changeProfile(driverDTO)
+        val driver = driverService.getDriverData(driverDTO.id)
+        driverService.changeProfile(driverDTO, driver)
+        return driverService.update(driver)
     }
 
     @GetMapping("/img")
-    fun getImg(@RequestParam driverId: Int): Map<String,String>{
-        return mapOf("img" to driverService.getImg(driverId))
+    fun getImg(@RequestParam driverid: Int): Map<String,String>{
+        val driver = driverService.getDriverData(driverid)
+        return mapOf("img" to driver.img)
     }
 }
--- src/main/kotlin/ar/edu/unsam/phm/uberto/services/DriverService.kt
+++ src/main/kotlin/ar/edu/unsam/phm/uberto/services/DriverService.kt
@@ -10,16 +10,13 @@ import exceptions.BusinessException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 class DriverService(val driverRepo: DriverRepository, val timeTripsService: TravelTimeMockService) {
-    fun getDriversAvailable(date: DateDTO): DriverCardAndTimeDTO{
-        val timeMap = timeTripsService.getTime() //service time
-        if(timeMap["time"] == null){
-            throw Exception("Error en el servicio de consulta de tiempo")
-        }
-        val cardDriverDTO = driverRepo.avaliable(date.date, timeMap["time"]!!).map{it.toCardDTO(timeMap["time"]!!, date.numberPassengers)}
-        return DriverCardAndTimeDTO(timeMap["time"]!!, cardDriverDTO) //o un MAP
+
+    fun getDriversAvailable(date: LocalDateTime, time: Int): List<Driver>{
+        return driverRepo.avaliable(date, time)
     }
 
     fun getDriverData(userID:Int):Driver{
@@ -27,31 +24,24 @@ class DriverService(val driverRepo: DriverRepository, val timeTripsService: Trav
         return driver
     }
 
-    fun getAllDrivers(): List<Driver> {
-        return driverRepo.instances.toMutableList()
-    }
-
-    fun changeProfile(driverDTO: DriverDTO): ResponseEntity<String> {
-        val driver = driverRepo.searchByUserID(driverDTO.id)
-        if(driver == null) throw BusinessException("Driver no encontrado")
-        mapFieldProfile(driverDTO, driver)
-        driverRepo.update(driver)
-        return ResponseEntity
-            .status(HttpStatus.OK)
-            .body("Perfil actualizado")
-    }
-
-    private fun mapFieldProfile(driverDTO: DriverDTO, driver: Driver){
+    fun changeProfile(driverDTO: DriverDTO, driver: Driver): ResponseEntity<String> {
         driver.firstName = driverDTO.firstName
         driver.lastName = driverDTO.lastName
         driver.serial = driverDTO.serial
         driver.brand = driverDTO.brand
         driver.model = driverDTO.model
         driver.balance = driverDTO.price
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body("Updated profile")
     }
 
-    fun getImg(driverId: Int): String {
-        val driver = driverRepo.searchByUserID(driverId)
-        return driver!!.img
-    }
+   fun update(driver: Driver): ResponseEntity<String>{
+       //muchas dudas con esto, quiero saber si realmente se lleva a cabo o no
+       driverRepo.update(driver)
+       return ResponseEntity
+               .status(HttpStatus.OK)
+               .body("Updated")
+   }
+
 }
