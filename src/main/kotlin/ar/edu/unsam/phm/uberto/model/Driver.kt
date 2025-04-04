b508493792c7680a044a185ec37e1966e666214b
diegolentz <diegoolentz@gmail.com>
2025-04-04 14:35:16 -0300

    descomento metodos driver

--- src/main/kotlin/ar/edu/unsam/phm/uberto/model/Driver.kt
+++ src/main/kotlin/ar/edu/unsam/phm/uberto/model/Driver.kt
@@ -49,21 +49,21 @@ abstract class Driver():User {
 
 
 
-//    fun avaliable(tripDate: LocalDateTime, tripDuration: Int):Boolean{
-//        val finishedDate =  tripDate.plus(tripDuration.toLong(), ChronoUnit.MINUTES)
-//        return !this.tripOverlapping(tripDate, finishedDate) || trips.isEmpty()
-//    }
+    fun avaliable(tripDate: LocalDateTime, tripDuration: Int):Boolean{
+        val finishedDate =  tripDate.plus(tripDuration.toLong(), ChronoUnit.MINUTES)
+        return !this.tripOverlapping(tripDate, finishedDate) || trips.isEmpty()
+    }
 
-//    fun tripOverlapping(tripStart: LocalDateTime, tripEnd: LocalDateTime):Boolean{
-//        return this.pendingTrips().any{ pending:Trip ->
-//            tripStart < pending.finalizationDate() && tripEnd > pending.date
-//        }
-//    }
+    fun tripOverlapping(tripStart: LocalDateTime, tripEnd: LocalDateTime):Boolean{
+        return this.pendingTrips().any{ pending:Trip ->
+            tripStart < pending.finalizationDate() && tripEnd > pending.date
+        }
+    }
 
-//    fun scoreAVG():Double{
-//        val avg = getScoredTrips().map { it.score!!.scorePoints }.average()
-//        return if(avg.isNaN()) 0.0 else avg
-//    }
+    fun scoreAVG():Double{
+        val avg = getScoredTrips().map { it.score!!.scorePoints }.average()
+        return if(avg.isNaN()) 0.0 else avg
+    }
 
     fun fee(time: Int, numberPassenger: Int):Double{
         return this.basePrice + this.plusBasePrice(time, numberPassenger) * time
@@ -71,32 +71,32 @@ abstract class Driver():User {
 
     abstract fun plusBasePrice(time: Int, numberPassengers: Int):Double
 
-//    override fun getScores(): List<TripScore> {
-//        return this.getScoredTrips().map{ it.score!! }
-//    }
+    override fun getScores(): List<TripScore> {
+        return this.getScoredTrips().map{ it.score!! }
+    }
 
-//    private fun getScoredTrips():List<Trip>{
-//        return this.trips.filter { it.score != null }
-//    }
+    private fun getScoredTrips():List<Trip>{
+        return this.trips.filter { it.score != null }
+    }
 
-//    fun addTrip(trip:Trip) {
-//        this.trips.add(trip)
-//        acceditTrip(trip.price())
-//    }
+    fun addTrip(trip:Trip) {
+        this.trips.add(trip)
+        acceditTrip(trip.price())
+    }
 
     fun acceditTrip(price: Double){
         this.balance += price
     }
 
-//    fun pendingTrips() = trips.filter { trip:Trip ->trip.pendingTrip()}
-//    fun finishedTrips() = trips.filter { trip:Trip ->trip.finished()}
-//
-//    fun responseTrip(newTrip: Trip, time: Int) {
-//        if(!avaliable(newTrip.date, time)){
-//            throw DriverNotAvaliableException()
-//        }
-//        addTrip(newTrip)
-//    }
+    fun pendingTrips() = trips.filter { trip:Trip ->trip.pendingTrip()}
+    fun finishedTrips() = trips.filter { trip:Trip ->trip.finished()}
+
+    fun responseTrip(newTrip: Trip, time: Int) {
+        if(!avaliable(newTrip.date, time)){
+            throw DriverNotAvaliableException()
+        }
+        addTrip(newTrip)
+    }
 
 }
 
