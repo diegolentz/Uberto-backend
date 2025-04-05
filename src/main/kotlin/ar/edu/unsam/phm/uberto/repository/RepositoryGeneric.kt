de7a7441d8d9eef33f84a0baa661f2be67989910
Diaz Matias Hernan <diaz.matiash@gmail.com>
2025-04-04 23:50:01 -0300

    se commenta repoTrip

--- src/main/kotlin/ar/edu/unsam/phm/uberto/repository/RepositoryGeneric.kt
+++ src/main/kotlin/ar/edu/unsam/phm/uberto/repository/RepositoryGeneric.kt
@@ -77,19 +77,19 @@ class PassengerRepository(): Repository<Passenger>() {
 
 }
 
-@Component
-class TripsRepository(): Repository<Trip>() {
-
-    fun searchByForm(form: FormTripDTO, driverId: Int): List<Trip>{
-        val tripFromDriver = instances.filter { it.driver.userId == driverId }
-        return tripFromDriver.filter{ trip ->
-            (form.origin == trip.origin || form.origin == null || form.origin == "") &&
-            (form.destination == trip.destination || form.destination == null || form.destination == "") &&
-            (form.numberPassengers == trip.numberPassengers || form.numberPassengers == null) &&
-            (form.name == trip.client.firstName || form.name == null || form.name == "")
-        }
-    }
-}
+//@Component
+//class TripsRepository(): Repository<Trip>() {
+//
+//    fun searchByForm(form: FormTripDTO, driverId: Int): List<Trip>{
+//        val tripFromDriver = instances.filter { it.driver.userId == driverId }
+//        return tripFromDriver.filter{ trip ->
+//            (form.origin == trip.origin || form.origin == null || form.origin == "") &&
+//            (form.destination == trip.destination || form.destination == null || form.destination == "") &&
+//            (form.numberPassengers == trip.numberPassengers || form.numberPassengers == null) &&
+//            (form.name == trip.client.firstName || form.name == null || form.name == "")
+//        }
+//    }
+//}
 
 @Component
 class TripScoreRepository(): Repository<TripScore>() {
