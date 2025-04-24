4afd915e19180eb05e2054b8b14abf8e97ff5cb1
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2025-04-23 23:08:16 -0300

    test de creacion de calificacion ok

--- src/test/kotlin/ar/edu/unsam/phm/uberto/controller/TripScoreSpringTest.kt
+++ src/test/kotlin/ar/edu/unsam/phm/uberto/controller/TripScoreSpringTest.kt
@@ -1,10 +1,12 @@
 package ar.edu.unsam.phm.uberto.controller
 
+import ar.edu.unsam.phm.uberto.dto.TripScoreDTO
 import ar.edu.unsam.phm.uberto.factory.TestFactory
 import ar.edu.unsam.phm.uberto.repository.DriverRepository
 import ar.edu.unsam.phm.uberto.repository.PassengerRepository
 import ar.edu.unsam.phm.uberto.repository.TripsRepository
 import ar.edu.unsam.phm.uberto.services.DriverService
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -51,5 +53,35 @@ class TripScoreSpringTest {
             .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(0))
     }
 
+    @Test
+    fun `Si creo una calificacion tanto el pasajero como el conductor podran ver la misma`() {
+        val trip = testFactory.createTripFinished(1).get(0)
+        val passenger = trip.client
+        val driver = trip.driver
+
+        passengerRepository.save(passenger)
+        driverRepository.save(driver)
+        tripRepository.save(trip)
+
+        val tripScore = TripScoreDTO(
+             trip.id!!,
+             "Viaje copado",
+             3,
+             "30/05/2025",
+             "Pica",
+             "Colorado",
+             "",
+             "",
+             false
+        )
+
+        val objectMapper = ObjectMapper()
+        val createInfoJson = objectMapper.writeValueAsString(tripScore)
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/tripScore")
+            .contentType("application/json")
+            .content(createInfoJson))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+    }
 
 }
