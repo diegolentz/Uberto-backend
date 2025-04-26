a63f410096e2db746bb2d8457b01d1986b85bb1d
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2025-04-26 16:27:26 -0300

    se cambia el cascade type a merge

--- src/main/kotlin/ar/edu/unsam/phm/uberto/model/Trip.kt
+++ src/main/kotlin/ar/edu/unsam/phm/uberto/model/Trip.kt
@@ -39,7 +39,7 @@ class Trip(
     @JoinColumn(name = "driver_id", referencedColumnName = "id")
     var driver: Driver = SimpleDriver()
 
-    @OneToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL],  orphanRemoval = true)
+    @OneToOne(fetch = FetchType.LAZY, cascade = [CascadeType.MERGE],  orphanRemoval = true)
     @JoinColumn(name = "tripscore_id", referencedColumnName = "id")
     var score: TripScore? = null
 
