197409796f93fd04a2fc5e63bc3510c6a324afae
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2025-04-27 18:08:45 -0300

    se implemento una vista para realizar el punto 5 del ejercicio

---
+++ src/main/kotlin/ar/edu/unsam/phm/uberto/sql/parte2_5.kt
@@ -0,0 +1,11 @@
+package ar.edu.unsam.phm.uberto.sql
+
+val VIEW_DRIVER_WITH_MORE_THAN_TWO_TRIPS = """
+    CREATE OR REPLACE VIEW choferes_con_mas_de_2_viajes_finalizados AS
+        SELECT d.id AS driver_id, d.first_name, d.last_name, COUNT(t.id) AS cantidad_viajes_finalizados
+        FROM driver d
+        JOIN trip t ON d.id = t.driver_id
+        WHERE (t.date + INTERVAL '1 minute' * t.duration) < NOW()
+        GROUP BY d.id, d.first_name, d.last_name
+        HAVING COUNT(t.id) > 2;  
+"""
