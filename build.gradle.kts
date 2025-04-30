f0105181ad8b1fd3b74e3db2157bd163c406c2eb
AdrianPerez0306 <perezadrian.m@hotmail.com>
2025-04-30 14:44:15 -0300

    Dependencias de Spring Security

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,13 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	runtimeOnly("org.postgresql:postgresql")
 
+//	SECURITY
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.bouncycastle:bcprov-jdk15on:1.70")
+	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+	implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+
+	implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
 //	TESTING
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
