82bbf95b118b667c2af98a2e21332aa539941a40
Diaz Matias Hernan <diaz.matiash@gmail.com>
2025-06-01 18:11:10 -0300

    se cambia de mongodriver a mongo

--- src/main/kotlin/ar/edu/unsam/phm/uberto/model/Driver.kt
+++ src/main/kotlin/ar/edu/unsam/phm/uberto/model/Driver.kt
@@ -9,7 +9,7 @@ import org.springframework.data.annotation.TypeAlias
 import org.springframework.data.mongodb.core.mapping.Document
 
 
-@Document(collection = "mongodriver")
+@Document(collection = "driver")
 abstract class Driver():User {
 
     @Id
